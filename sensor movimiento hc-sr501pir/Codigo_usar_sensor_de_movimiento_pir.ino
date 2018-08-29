9d005e2a0df77510b3b4a5f93b59508d00167983
DanielCamelCase <42586858+DanielCamelCase@users.noreply.github.com>
2018-08-29 21:41:55 +0200

    Update Codigo_usar_sensor_de_movimiento_pir.ino

--- sensor movimiento hc-sr501pir/Codigo_usar_sensor_de_movimiento_pir.ino
+++ sensor movimiento hc-sr501pir/Codigo_usar_sensor_de_movimiento_pir.ino
@@ -1,34 +1,34 @@
 
 
-const int sPIR = 7; // Sensor de movimiento PIR | Pin 8 - Arduino Uno;
-const int led = 9; // Led | Pin 13 - Arduino Uno;
+const int sPIR = 7; // Sensor de movimiento PIR HC-SR501 / Pin 8 - Arduino Uno;
+const int led = 9; // Led / Pin 13 - Arduino Uno;
 
-int mov = 0; // Variable que guarda el cambio de estado del PIR
+int mov = 0; // Variable que guarda el cambio de estado del PIR HC-SR501
 
 void setup() {
   Serial.begin(9600);
   pinMode(led,OUTPUT);
   pinMode(sPIR,INPUT);
-  Serial.println("PIR Motion | Sensor de Movimiento IR");
-  Serial.println("Escrito por Hugo Oroño para iobot");
+  Serial.println("PIR HC-SR501| Sensor de Movimiento Infrarrojos");
+  Serial.println("Escrito por DanielCamelCase");
   Serial.println(" ");
 }
 
 void loop() {
   mov = digitalRead(sPIR);
-  delay(50);
+  delay(100);
   if (mov == HIGH) {
     Serial.print(mov); Serial.print(" : "); Serial.println("1"); 
     digitalWrite(led,HIGH);
-    delay(8000);
+    delay(7000);
     digitalWrite(led,LOW);
     mov = 0;
-    delay(300);
+    delay(200);
   }
     
   else {
-    Serial.print(mov); Serial.print(" : "); Serial.println("No se detecta movimiento");
-    delay(300);
+    Serial.print(mov); Serial.print(" : "); Serial.println("No detecto ningun movimiento");
+    delay(200);
     }
     }
 
